a43f1112d54ab997ce58b87cb7c78f1067c1d294
Maxout0 <147980070+Maxout0@users.noreply.github.com>
2026-02-08 06:01:24 -0800

    Add burger menu toggle functionality

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-git 
+<script>
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+</script>
