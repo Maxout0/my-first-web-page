d36e014015b2ad15108a43fbaec861725dacc147
Maxout0 <147980070+Maxout0@users.noreply.github.com>
2026-02-08 17:46:02 +0100

    Fix script tag formatting and remove extra lines

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-<script>
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
-</script>
