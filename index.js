c675d46ce53a3f3d5cb38f58788800cbf641587c
Maxout0 <147980070+Maxout0@users.noreply.github.com>
2026-02-11 15:10:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-  const burger = document.querySelector('.burger');
+ const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('active');
-  });
+   });
 
 if (window.innerWidth <= 768){
   const nav =  document.querySelector(".nav-links");
-  const dropDown = document.querySelector(".dropdwon");
+  const dropDown = document.querySelector(".dropdown");
   const aboutLink = dropDown?.querySelector('a[href*="about"]');
-  const contactlink = dropDown?.querySelector(".dropdown-menu a");
+  const contactLink = dropDown?.querySelector(".dropdown-menu a");
 
   if (nav && dropDown && aboutLink && contactLink) {
     const newAboutlink = aboutLink.cloneNode(true);
